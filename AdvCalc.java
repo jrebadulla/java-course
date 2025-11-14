a498c8b3688d85ec5a311fcdd8101e719908ee0b
Jellian <jellianrebadulla@gmail.com>
2025-11-14 14:59:19 +0800

    Java Inheritance

---
+++ AdvCalc.java
@@ -0,0 +1,11 @@
+public class AdvCalc extends Calc { // AdvCalc is a Super class and the Calc is the sub class
+
+    public int multi(int n1, int n2) {
+        return n1 * n2;
+    }
+
+    public int divide(int n1, int n2) {
+        return n1 / n2;
+    }
+
+}
